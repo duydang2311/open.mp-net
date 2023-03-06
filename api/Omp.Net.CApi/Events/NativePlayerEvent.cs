c0111f7e0d8652d3302a2f42f81fae59141b9205
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-03-06 12:00:04 +0700

    feat(NativePlayerEvent): add connect and spawn events

--- api/Omp.Net.CApi/Events/NativePlayerEvent.cs
+++ api/Omp.Net.CApi/Events/NativePlayerEvent.cs
@@ -2,19 +2,59 @@ using System.Runtime.InteropServices;
 
 namespace Omp.Net.CApi.Events;
 
-public delegate int PlayerRequestSpawnDelegate(int playerid);
+public delegate void OnIncomingConnectionDelegate(UnmanagedEntityId player, string ipAddress, ushort port);
+public delegate void OnPlayerConnectDelegate(UnmanagedEntityId player);
+public delegate void OnPlayerDisconnectDelegate(UnmanagedEntityId player, int reason);
+public delegate void OnPlayerClientInitDelegate(UnmanagedEntityId player);
+public delegate int OnPlayerRequestSpawnDelegate(UnmanagedEntityId player);
+public delegate void OnPlayerSpawnDelegate(UnmanagedEntityId player);
 
 internal static partial class NativePlayerEvent
 {
-	public static event PlayerRequestSpawnDelegate? NativePlayerRequestSpawnEvent;
+	public static event OnIncomingConnectionDelegate? NativeOnIncomingConnection;
+	public static event OnPlayerConnectDelegate? NativeOnPlayerConnect;
+	public static event OnPlayerDisconnectDelegate? NativeOnPlayerDisconnect;
+	public static event OnPlayerClientInitDelegate? NativeOnPlayerClientInit;
+	public static event OnPlayerRequestSpawnDelegate? NativeOnPlayerRequestSpawn;
+	public static event OnPlayerSpawnDelegate? NativeOnPlayerSpawn;
 
 	[UnmanagedCallersOnly()]
-	private static int PlayerRequestSpawn(int playerid)
+	private static void OnIncomingConnection(UnmanagedEntityId player, IntPtr ipAddress, ushort port)
 	{
-		if (NativePlayerRequestSpawnEvent is null)
+		NativeOnIncomingConnection?.Invoke(player, Marshal.PtrToStringAnsi(ipAddress)!, port);
+	}
+
+	[UnmanagedCallersOnly()]
+	private static void OnPlayerConnect(UnmanagedEntityId player)
+	{
+		NativeOnPlayerConnect?.Invoke(player);
+	}
+
+	[UnmanagedCallersOnly()]
+	private static void OnPlayerDisconnect(UnmanagedEntityId player, int reason)
+	{
+		NativeOnPlayerDisconnect?.Invoke(player, reason);
+	}
+
+	[UnmanagedCallersOnly()]
+	private static void OnPlayerClientInit(UnmanagedEntityId player)
+	{
+		NativeOnPlayerClientInit?.Invoke(player);
+	}
+
+	[UnmanagedCallersOnly()]
+	private static int OnPlayerRequestSpawn(UnmanagedEntityId player)
+	{
+		if (NativeOnPlayerRequestSpawn is null)
 		{
 			return 1;
 		}
-		return NativePlayerRequestSpawnEvent.Invoke(playerid);
+		return NativeOnPlayerRequestSpawn.Invoke(player);
+	}
+
+	[UnmanagedCallersOnly()]
+	private static void OnPlayerSpawn(UnmanagedEntityId player)
+	{
+		NativeOnPlayerSpawn?.Invoke(player);
 	}
 }
